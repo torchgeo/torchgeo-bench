e279f0406229e4586eb999b60fb587a408aa5e88
Isaac Corley <isaac.a.corley@gmail.com>
2026-09-04 18:28:07 -0500

    style(commands): order lazy profile exports

--- src/torchgeo_bench/commands/__init__.pyi
+++ src/torchgeo_bench/commands/__init__.pyi
@@ -1,6 +1,6 @@
 from ._download import download as download
 from ._flops import flops as flops
-from ._run import run as run
 from ._profile import profile as profile
+from ._run import run as run
 
 __all__: tuple[str, ...]
